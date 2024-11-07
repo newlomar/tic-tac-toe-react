37bdb98f839bc35f4e30ffcf2a75de34e9677c05
Newton Lomar <40671286+newlomar@users.noreply.github.com>
2024-11-07 01:18:06 -0300

    adding matrix type

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -67,7 +67,9 @@ function Board() {
 
 export default function Game() {
   const [xIsNext, setXIsNext] = useState(true);
-  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [history, setHistory] = useState<(string | null)[][]>([
+    Array(9).fill(null),
+  ]);
   const currentSquares = history[history.length - 1];
   return (
     <div className="game">
