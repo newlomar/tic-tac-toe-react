8fafda0b2495e3efa365d0584c8e105f79e6eb4e
Newton Lomar <40671286+newlomar@users.noreply.github.com>
2024-11-08 02:25:40 -0300

    changing handleplay to track only until current move

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -77,7 +77,9 @@ export default function Game() {
   console.log(history);
 
   function handlePlay(nextSquares: (string | null)[]) {
-    setHistory([...history, nextSquares]);
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
     setXIsNext(!xIsNext);
   }
 
